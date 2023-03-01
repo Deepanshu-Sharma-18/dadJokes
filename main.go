b249272b7ce948c0b729483212e4cc944b9c39ae
Deepanshu-Sharma-18 <deepanshu.r.sharma@gmail.com>
2023-03-01 14:40:51 +0530

    edit

--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"os"
+
 	"github.com/Deepanshu-Sharma-18/api/database"
 	"github.com/Deepanshu-Sharma-18/api/operations"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load(".env")
 	database.StoreJokes()
 	router := gin.Default()
 	router.GET("/jokes", operations.GetJoke)
 	router.GET("/random", operations.GetRandom)
 	router.GET("/jokes/:id", operations.GetJokewithId)
 	router.POST("/addJoke", operations.PostJoke)
-	router.Run("localhost:8080")
+	port := os.Getenv("PORT")
+	router.Run("localhost:%v", port)
 }
